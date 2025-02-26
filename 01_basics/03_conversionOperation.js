77c136ef602a50c490b4b6ac4379ac24f329f09e
ASHISH KUMAR <ashishk900655@gmail.com>
2025-02-26 18:35:21 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score ="55"
+
+let valuInNumber =Number(score)
+console.log(typeof valuInNumber)
+//"33"=>33
+//"33dfd"=>NaN
+//true=>1,false=>0
+
+let isLoggedIn="hitesh"
+let boolenIsLoggedIn=Boolean(isLoggedIn)
+console.log(boolenIsLoggedIn)
+//1=>true,0=>false
+//""=>false
+//"Ashish"=>true
