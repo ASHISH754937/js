d8dff6e728b6dec5d1a898ee47225e7861cab1d7
ASHISH KUMAR <ashishk900655@gmail.com>
2025-02-27 17:14:13 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,18 @@
+const name ="Ashsih"
+const repoCount=50
+
+//console.log(name + repoCount + "Value")//use it but it is not better
+
+console.log(`Hello muy name is ${name} and my repo is ${repoCount}`)
+const gameName=new String('Ashish')
+
+console.log(gameName[0])
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('h'))
+
+
+const newStringOne = "   Tara  "
+console.log(newStringOne)
+console.log(newStringOne.trim())
