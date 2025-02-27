68c10e0b18fd897b667d0ecf117312e0165f7ecb
ASHISH KUMAR <ashishk900655@gmail.com>
2025-02-27 17:14:34 +0000

    dataTypeSummary

---
+++ 01_basics/dataTypesSummary.js
@@ -0,0 +1,19 @@
+//primitive
+//7 types : string,Number,Boolearn,null,undefined , Symbol,BighInt
+
+//reference(Non Primitive)
+//Array, Objects,Functions
+
+
+//****************stack(Primitive)*****heap(non-Primitive)
+let myYoutubename="dfsdfsdffssfsfsf"
+let anothername=myYoutubename
+
+console.log(anothername) 
+
+let user={
+    email:"ashffh@gmail",
+    upi:"ffjf@ybl"
+}
+
+console.log(user.email)
