804eed191cfbb01c6b2645e2b7d99eddb3bd2fb1
ASHISH KUMAR <ashishk900655@gmail.com>
2025-03-01 18:14:43 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,18 @@
+const myArr = [0,1,2,3,4,5]
+const myHeors = ["shaktiman", "naagraj"]
+
+const myArr2=new Array(1,2,3,4)
+
+console.log(myArr[1]);
+
+//array methods
+
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
+console.log(myArr)
+myArr.unshift(9)
+console.log(myArr)
+myArr.shift()
+console.log(myArr)
+console.log(myArr.includes(9))
