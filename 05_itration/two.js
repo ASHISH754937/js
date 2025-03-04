662af52b12978b051f6dc257387f4bfa5cdc2112
ASHISH KUMAR <ashishk900655@gmail.com>
2025-03-04 18:28:56 +0000

    itration

---
+++ 05_itration/two.js
@@ -0,0 +1,14 @@
+let index=0
+while(index<=10)
+{
+    console.log(`Value of index is ${index}`);
+    index+=2;
+}
+
+
+
+let score=1;
+do{
+    console.log(`score is ${score}`)
+    score++;
+}while(score<=10)
